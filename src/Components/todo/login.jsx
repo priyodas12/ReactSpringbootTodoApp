e88f83923f48dfaa6906911e658da65e2e76af57
Priyobrato Das <priyodas12@gmail.com>
2020-04-04 22:07:02 +0530

    addde generic handler

--- src/Components/todo/login.jsx
+++ src/Components/todo/login.jsx
@@ -8,32 +8,27 @@ export class Login extends Component {
            username:'priyodas12@gmail.com',
            password:'ashjdkask'
          }
-         this.usernameChangeHandler=this.usernameChangeHandler.bind(this);
-         this.passwordChangeHandler=this.passwordChangeHandler.bind(this);
+         {/*this.usernameChangeHandler=this.usernameChangeHandler.bind(this);
+         this.passwordChangeHandler=this.passwordChangeHandler.bind(this);*/}
+         this.loginEventHandler=this.loginEventHandler.bind(this)
        }
 
-       usernameChangeHandler(event){
-         console.log(event.target.value)
-         this.setState({
-           [event.target.name]:event.target.value
-         })
-       }
+        loginEventHandler(event){
+            console.log(event.target.value)
+                this.setState({
+                  [event.target.name]:event.target.value
+                })
+              }
 
-       passwordChangeHandler(event){
-         console.log(event.target.value)
-         this.setState({
-           [event.target.name]:event.target.value
-         })
-       }
-
+       
        render() {
           return (
             <div className='login'>
                 <p>Login Component</p>
                 Username:
-                <input type='text' name='username' value={this.state.username} onChange={this.usernameChangeHandler}/><br/><br/>
+                <input type='text' name='username' value={this.state.username} onChange={this.loginEventHandler}/><br/><br/>
                 Password:
-                <input type='password' name='password' value={this.state.password} onChange={this.passwordChangeHandler}/> <br/><br/>
+                <input type='password' name='password' value={this.state.password} onChange={this.loginEventHandler}/> <br/><br/>
                 <button>login</button>
             </div>
         )
