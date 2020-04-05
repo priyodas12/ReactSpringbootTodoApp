ef32114ddf8816096d4ca43a3ed0d47ccb675565
Priyobrato Das <priyodas12@gmail.com>
2020-04-05 19:45:44 +0530

    committed new springboot app

---
+++ todo_SpringbootApp/src/main/java/todo_SpringbootApp/TodoSpringbootAppApplication.java
@@ -0,0 +1,13 @@
+package todo_SpringbootApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoSpringbootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoSpringbootAppApplication.class, args);
+	}
+
+}
---
+++ todo_SpringbootApp/src/main/java/todo_SpringbootApp/controller/HelloController.java
@@ -0,0 +1,18 @@
+package todo_SpringbootApp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+	
+	@GetMapping("/get")
+	public String getMessage() {
+		return "Hello";
+	}
+	
+	
+
+}
